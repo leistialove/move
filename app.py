81a6d5aaa0649e8fc79adebf53223c50623ec718
leistialove <106289664+leistialove@users.noreply.github.com>
2025-06-24 11:20:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def update_status():
     
     return "OK"
 
-MJPEG_SOURCE = "https://lack-centres-focal-considering.trycloudflare.com//video_feed"  # 換成 cloudflare 給的網址+/video_feed
+MJPEG_SOURCE = "https://authorization-schedule-im-why.trycloudflare.com/video_feed"  # 換成 cloudflare 給的網址+/video_feed
 
 @app.route('/stream')
 def stream():
